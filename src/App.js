8750c23b9e9f2e3ea85b2dee22f14dd1e1c80088
jfrummel <jfrummel@gmail.com>
2019-01-28 10:40:41 -0600

    HTTP and API setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HttpService from './services/http-service';
+
+const http = new HttpService();
 
 class App extends Component {
+
+  constructor (props) {
+    super(props);
+    this.loadData = this.loadData.bind(this);
+    this.loadData();
+  };
+
+  loadData = () => {
+    http.getProducts().then(data => {
+      console.log(data);
+    }, err => {
+
+    
+    });
+  };
+
   render() {
     return (
       <div className="container App">
